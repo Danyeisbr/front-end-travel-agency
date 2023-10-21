aca1a727c878d494b60b57264f3541aef23490ef
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-21 03:23:55 -0500

    packages updated

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://test-17.azurewebsites.net/api/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+console.log(process.env.REACT_APP_API_BASE_URL);
 
 export default axiosInstance;
