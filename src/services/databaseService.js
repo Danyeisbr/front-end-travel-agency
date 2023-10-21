312b15aae9e84a6b7e117ab4b5dd5ac0b5c854c5
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-21 14:42:07 -0500

    url backend

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://test-17.azurewebsites.net/api/",
+  baseURL: "https://api-travel-app.azurewebsites.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-console.log(process.env.REACT_APP_API_BASE_URL);
 
 export default axiosInstance;
